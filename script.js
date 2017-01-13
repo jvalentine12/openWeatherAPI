0825cf6d48d6ad9797a0285896182078fd7f6c24
jvalentine12 <jvalentine@cristoreyny.org>
2017-01-13 13:49:29 +0000

    weather api

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#go").click(function(){
+       
+        var searchval= $("#search").val();
+        var part1= "https://api.apixu.com/v1/current.json?"
+        var part2= "key=a90bf5c92af543218bd212238170801&q="
+        var URL= part1 + part2 + searchval ;
+        
+        $.getJSON(URL,function(response){
+            var temperature= response.current.temp_f;
+            var humidity= response.current.humidity;
+            var windspeed= response.current.wind_mph;
+        $("#weather-results").append(function(){
+            
+        });
+        });
+    });
+});
+
